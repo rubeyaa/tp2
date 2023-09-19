d29bd3ddfe71da15fbed25be03e16d524561e7c6
rubeyaa <143745964+rubeyaa@users.noreply.github.com>
2023-09-19 11:44:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #créé par Adam Rubeya
 #créé en 2023
-#Afficher le nombre de charactère dans une phrase
+#Afficher le nombre de mots dans une phrase
 
 
 def count_word(charactere):
     return len (charactere.split(" "))
 #ici je défini la variable qui me permet de compter le nombre de mots
 
-count_word()
